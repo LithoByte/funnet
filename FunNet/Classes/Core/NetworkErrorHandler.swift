a50fd56f9492b45ea92d04f7411c643e2b275787
Elliot <elliot.schrock@gmail.com>
2021-05-11 14:36:45 -0400

    add url loading error handling

--- FunNet/Classes/Core/NetworkErrorHandler.swift
+++ FunNet/Classes/Core/NetworkErrorHandler.swift
@@ -13,6 +13,12 @@ public protocol ErrorMessage {
     var forCode: Int { get }
 }
 
+public struct SimpleErrorMessage: ErrorMessage {
+    public var title: String
+    public var message: String
+    public var forCode: Int
+}
+
 public protocol NetworkErrorHandler {
     func alert(for error: NSError) -> UIViewController
 }
@@ -41,23 +47,23 @@ public class VerboseNetworkErrorHandler: NetworkErrorHandler {
     open func alert(for error: NSError) -> UIViewController {
         print(error)
         if let message = errorMessageMap[error.code] {
-            return alert(message.title, message.message)
+            return FunNet.alert(message.title, message.message)
         } else {
-            return alert("Error \(error.code)", "Description: \(error.debugDescription)\nInfo: \(error.userInfo)")
+            return FunNet.alert("Error \(error.code)", "Description: \(error.debugDescription)\nInfo: \(error.userInfo)")
         }
     }
     
     open func notify(title: String, message: String) {
-        alert(title, message).show(animated: true)
+        FunNet.alert(title, message).show(animated: true)
     }
-    
-    func alert(_ title: String, _ message: String) -> UIAlertController {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
-            alert.dismiss(animated: true, completion: nil)
-        }))
-        return alert
-    }
+}
+
+public func alert(_ title: String, _ message: String) -> UIAlertController {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
+        alert.dismiss(animated: true, completion: nil)
+    }))
+    return alert
 }
 
 public struct DefaultServerUnavailableErrorMessage: ErrorMessage {
