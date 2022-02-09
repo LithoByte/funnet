696ddd53d10d366dd3dd6ea8c8ca2d129bdf2975
Calvin Collins <calvin.john.collins@gmail.com>
2022-02-09 11:39:26 -0500

    started adding subpackages

--- Package.swift
+++ Package.swift
@@ -10,6 +10,24 @@ let package = Package(
         .library(
             name: "FunNet",
             targets: ["FunNet"]),
+        .library(
+            name: "FunNet/Core",
+            targets: ["Core"]),
+        .library(
+            name: "FunNet/Combine",
+            targets: ["Combine"]),
+        .library(
+            name: "FunNet/ReactiveSwift",
+            targets: ["FunNetReactiveSwift"]),
+        .library(
+            name: "FunNet/Multipart",
+            targets: ["Multipart"]),
+        .library(
+            name: "FunNet/ErrorHandling",
+            targets: ["ErrorHandling"]),
+        .library(
+            name: "FunNet/ErrorHandlingCombine",
+            targets: ["ErrorHandlingCombine"])
     ],
     dependencies: [
         .package(url: "https://github.com/LithoByte/LithoOperators.git", .branch("master")),
@@ -20,6 +38,38 @@ let package = Package(
     targets: [
         .target(
             name: "FunNet",
-            dependencies: ["LithoOperators", .productItem(name: "LithoUtils", package: "LithoUtils/Post13", condition: nil), "Slippers", "ReactiveSwift"]),
+            dependencies: ["LithoOperators", .productItem(name: "LithoUtils", package: "LithoUtils/Post13", condition: nil), "Slippers", "ReactiveSwift"],
+            path: "Sources/funnet"
+        ),
+        .target(
+            name: "Core",
+            dependencies: ["LithoOperators", "Slippers"],
+            path: "Sources/funnet/Core"
+        ),
+        .target(
+            name: "Combine",
+            dependencies: [.targetItem(name: "Core", condition: nil)],
+            path: "Sources/funnet/Combine"
+        ),
+        .target(
+            name: "FunNetReactiveSwift",
+            dependencies: [.targetItem(name: "Core", condition: nil), "ReactiveSwift"],
+            path: "Sources/funnet/ReactiveSwift"
+        ),
+        .target(
+            name: "Multipart",
+            dependencies: ["LithoOperators"],
+            path: "Sources/funnet/Multipart"
+        ),
+        .target(
+            name: "ErrorHandling",
+            dependencies: ["LithoOperators", .productItem(name: "LithoUtils", package: "LithoUtils/Post13", condition: nil), "Slippers"],
+            path: "Sources/funnet/ErrorHandling"
+        ),
+        .target(
+            name: "ErrorHandlingCombine",
+            dependencies: [.targetItem(name: "ErrorHandling", condition: nil), "LithoOperators", "Slippers", .productItem(name: "LithoUtils", package: "LithoUtils/Post13", condition: nil)],
+            path: "Sources/funnet/ErrorHandlingCombine"
+        ),
     ]
 )
